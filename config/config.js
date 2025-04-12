ed1607fd59aa1d11ddcb5e98abe1736c5cb49ee2
bowed89 <jesus.lopez.averanga@gmail.com>
2025-04-12 01:35:44 -0400

    cambio de puerto y package

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ require("dotenv").config();
 };
  */
 
-module.exports = {
+/* module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
@@ -41,3 +41,21 @@ module.exports = {
     },
   },
 };
+ */
+
+module.exports = {
+  production: {
+    username: "tareas_db_c8au_user", // Usuario de tu base de datos en Render
+    password: "5slSGQZYscRm0hNy8VhLG1423VDSgnLk", // Contraseña de tu base de datos en Render
+    database: "tareas_db_c8au", // Nombre de la base de datos
+    host: "dpg-cvsve1pr0fns73ds2glg-a.oregon-postgres.render.com", // Host de la base de datos
+    dialect: "postgres", // Esto es importante
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Necesario para Render
+      },
+    },
+  },
+};
+
